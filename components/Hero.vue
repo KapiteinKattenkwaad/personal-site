b4b51750fc65c1aeb099b28a1a116f31dbabc84d
Max Stouten <max.stouten@lunar.be>
2022-12-12 18:49:36 +1100

    changed hero

--- components/Hero.vue
+++ components/Hero.vue
@@ -5,12 +5,12 @@
           <div class="col-md-7 my-auto">
             <div>
                 <p class="hero-text">
-                    <span>I’m Raj, a Frontend Developer.&nbsp;Always interested in Graphics, I started from a little coding knowledge, went on to designing stuff and later shifted to Frontend Development. Now, I'm a full-time Vue.js Developer.&nbsp;</span>
+                    <span>I’m Max, a Frontend Developer.&nbsp;Always interested in Graphics, I started from a little coding knowledge, went on to designing stuff and later shifted to Frontend Development. Now, I'm a full-time Vue.js Developer.&nbsp;</span>
                 </p>
             </div>
           </div>
           <div class="col-md-5">
-              <img src="~/assets/img/hero-img.png" alt="Raj Kumar Shrestha" class="img-fluid">
+              <img src="~/assets/img/profile-pic.png" alt="Max Stouten Web Developer" class="img-fluid">
           </div>
         </div>
       </div>
